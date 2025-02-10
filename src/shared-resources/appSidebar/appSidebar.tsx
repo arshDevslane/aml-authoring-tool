d113f634c6f2d77f67452e8db5a87deba277f1b3
arshDevslane <139157393+arshDevslane@users.noreply.github.com>
2025-02-10 17:06:41 +0530

    showing repository in sidebar (#50)
    Co-authored-by: Arsh <arsh@Arshs-MacBook-Air.local>

--- src/shared-resources/appSidebar/appSidebar.tsx
+++ src/shared-resources/appSidebar/appSidebar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   ChevronUp,
   FileText,
+  FolderGit2,
   Layers,
   TableOfContents,
   User2,
@@ -47,11 +48,11 @@ const items = [
     url: '/app/content',
     icon: TableOfContents,
   },
-  // {
-  //   title: 'Repository',
-  //   url: '/app/repository',
-  //   icon: FolderGit2,
-  // },
+  {
+    title: 'Repository',
+    url: '/app/repository',
+    icon: FolderGit2,
+  },
 ];
 
 export const AppSidebar = () => {
