d502f77f3a07670d37684eba1037924002604e47
arshDevslane <139157393+arshDevslane@users.noreply.github.com>
2025-03-11 18:08:58 +0530

    Video multilang (#52)
    * video multi lang changes
    
    * added telugu and tamil in languages
    
    ---------
    
    Co-authored-by: Arsh <arsh@Arshs-Air.domain.name>

--- src/models/enums/SupportedLanguages.enum.ts
+++ src/models/enums/SupportedLanguages.enum.ts
@@ -2,10 +2,14 @@ export enum SupportedLanguages {
   EN = 'en',
   KN = 'kn',
   HI = 'hi',
+  TE = 'te',
+  TA = 'ta',
 }
 
 export enum SupportedLanguagesLabel {
-  en = 'English',
   kn = 'Kannada',
+  en = 'English',
   hi = 'Hindi',
+  ta = 'Tamil',
+  te = 'Telugu',
 }
